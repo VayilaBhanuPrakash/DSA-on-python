93c74331da2a2c3b113d4ad0c632bfec767b1c89
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-04-23 22:02:02 +0530

    Time: 47 ms (67.53%) | Memory: 34.5 MB (84.85%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -4,11 +4,6 @@ class Solution:
         l=[0]*n
         first=0
         maxx=2**maximumBit-1
-        """if nums[-1]==0:
-            for i in range(n):
-                first=first^nums[i]
-                l[n-i-1]=first^1
-            return l"""
         for i in range(n):
             first=first^nums[i]
             l[n-i-1]=first^maxx
