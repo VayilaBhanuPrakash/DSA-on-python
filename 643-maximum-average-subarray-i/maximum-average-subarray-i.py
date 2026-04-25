21a17155acbba6863ab934a0852a9a18356b9af8
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-04-25 11:55:32 +0530

    Time: 66 ms (49.91%) | Memory: 29.4 MB (8.61%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -6,4 +6,20 @@ class Solution:
             summ=summ-nums[i-k]+nums[i]
             maxx=max(maxx,summ)
         return maxx/k
+
+        """i=0
+        j=k
+        summ=0
+        for index in range(k):
+            summ+=nums[index]
+        maxx=summ
+        while j<len(nums):
+            summ-=nums[i]
+            summ+=nums[j]
+            j+=1
+            i+=1
+            maxx=max(maxx,summ)
+        return maxx/k"""
+
+
         
