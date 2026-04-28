04ada3bfc414dc81fcc0104f9c03285c1b07ba99
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-04-28 13:28:42 +0530

    Time: 399 ms (54.83%) | Memory: 19.5 MB (11.04%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -3,15 +3,24 @@ class Solution:
         nums.sort()
         res=[]
         n=len(nums)
-        for i in range(n):
-            for j in range(i+1,n):
+        for i in range(n-3):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            for j in range(i+1,n-2):
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
                 k=j+1
                 l=n-1
                 while k<l:
                     total=nums[i]+nums[j]+nums[k]+nums[l]
                     if total==target:
-                        if [nums[i],nums[j],nums[k],nums[l]] not in res:
-                            res.append([nums[i],nums[j],nums[k],nums[l]])
+                        """if [nums[i],nums[j],nums[k],nums[l]] not in res:
+                            res.append([nums[i],nums[j],nums[k],nums[l]])"""
+                        res.append([nums[i],nums[j],nums[k],nums[l]])
+                        while k<l and nums[k]==nums[k+1]:
+                            k+=1
+                        while k<l and nums[l]==nums[l-1]:
+                            l-=1
                         k+=1
                         l-=1
                     elif total>target:
