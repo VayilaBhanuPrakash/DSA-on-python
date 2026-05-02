ba58b0f3c4e3aa8e234e96018d11421cf86963c4
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-02 13:38:59 +0530

    Time: 43 ms (30.95%) | Memory: 19.3 MB (69.59%) - LeetSync

---
+++ 804-rotated-digits/rotated-digits.py
@@ -0,0 +1,28 @@
+class Solution:
+    def rotatedDigits(self, n: int) -> int:
+        count=0
+        if n<=10:
+            sett={2,5,6,9}
+            for i in range(1,n+1):
+                if i in sett:
+                    count+=1
+            return count
+        count=4
+        sett1={2,5,6,9}
+        sett2={0,1,8}
+        for i in range(12,n+1):
+            flag1=0
+            flag2=0
+            c=0
+            while i>0:
+                rem=i%10
+                i=i//10
+                c+=1
+                if rem in sett1:
+                    flag1+=1
+                elif rem in sett2:
+                    flag2+=1
+            if c==flag1+flag2 and flag1:
+                count+=1
+        return count
+        
