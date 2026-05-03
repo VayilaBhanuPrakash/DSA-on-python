deefacd887e7b42faa2b2a0f2568d14974236e0f
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-03 11:39:45 +0530

    Time: 7 ms (38.92%) | Memory: 19.2 MB (47.82%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,23 +1,22 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        dict_s_t={}
-        dict_t_s={}
+        """dict_s={}
+        dict_t={}
         for i in range(len(s)):
             first=s[i]
             second=t[i]
-            if first not in dict_s_t:
-                dict_s_t[first]=second
-            else:
-                if dict_s_t[first]!=second:
+            if first in dict_s:
+                if dict_s[first]!=second:
                     return False
-            if  second not in dict_t_s:
-                dict_t_s[second]=first
             else:
-                if dict_t_s[second]!=first:
+                dict_s[first]=second
+            if second in dict_t:
+                if dict_t[second]!=first:
                     return False
-        return True
-
-        """h={}
+            else:
+                dict_t[second]=first
+        return True"""
+        h={}
         for i in range(len(s)):
             #first=s[i]
             #second=t[i]
@@ -27,4 +26,6 @@ class Solution:
                 return False
             else:
                 h[s[i]]=t[i]
-        return True"""
+        return True
+
+            
