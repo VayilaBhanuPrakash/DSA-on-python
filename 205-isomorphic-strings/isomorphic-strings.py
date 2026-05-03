0721925cd06b3f1dd08c809f2d0c86879e5cd1b4
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-03 11:31:04 +0530

    Time: 3 ms (91.59%) | Memory: 19.4 MB (24.21%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,21 +1,18 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        dict_s={}
-        dict_t={}
+        dict_s_t={}
+        dict_t_s={}
         for i in range(len(s)):
             first=s[i]
             second=t[i]
-            if first in dict_s:
-                if dict_s[first]!=second:
-                    return False
+            if first not in dict_s_t:
+                dict_s_t[first]=second
             else:
-                dict_s[first]=second
-            if second in dict_t:
-                if dict_t[second]!=first:
+                if dict_s_t[first]!=second:
                     return False
+            if  second not in dict_t_s:
+                dict_t_s[second]=first
             else:
-                dict_t[second]=first
-        return True
-            
-
-            
+                if dict_t_s[second]!=first:
+                    return False
+        return True        
