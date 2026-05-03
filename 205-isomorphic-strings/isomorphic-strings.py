7dd3693f3e21a60bbe367272f7e2f8bfa850f818
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-03 11:45:23 +0530

    Time: 3 ms (91.59%) | Memory: 19.2 MB (47.82%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -16,33 +16,7 @@ class Solution:
                 if dict_t_s[second]!=first:
                     return False
         return True
-        """dict_s={}
-        dict_t={}
-        for i in range(len(s)):
-            first=s[i]
-            second=t[i]
-            if first in dict_s:
-                if dict_s[first]!=second:
-                    return False
-            else:
-                dict_s[first]=second
-            if second in dict_t:
-                if dict_t[second]!=first:
-                    return False
-            else:
-                dict_t[second]=first
-        return True
-        h={}
-        for i in range(len(s)):
-            #first=s[i]
-            #second=t[i]
-            if s[i] in h.keys() and h[s[i]]!=t[i]:
-                return False
-            elif s[i] not in h.keys() and t[i] in h.values():
-                return False
-            else:
-                h[s[i]]=t[i]
-        return True"""
+        
       
 
             
