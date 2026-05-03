823ee2cca313d5950b1e054f3b6a182a2e57c30a
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-03 15:56:38 +0530

    Time: 3 ms (55.72%) | Memory: 19.2 MB (48.78%) - LeetSync

--- 2083-three-divisors/three-divisors.py
+++ 2083-three-divisors/three-divisors.py
@@ -1,11 +1,9 @@
 class Solution:
     def isThree(self, n: int) -> bool:
-        i=1
-        count=0
-        while i<=n//2:
+        count=2
+        for i in range(2,n//2+1):
             if n%i==0:
                 count+=1
-            i+=1
-        return count==2
+        return count==3
 
         
