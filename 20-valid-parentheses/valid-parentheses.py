8244319981f18e5e629454898ff329c7092a5930
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-04 15:52:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (5.95%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -12,28 +12,4 @@ class Solution:
                 if ele=="}" and (len(stack)==0 or stack.pop()!="{"):
                     return False
         return len(stack)==0
-
-
-
-
-
-
-
-
-
-
-
-
-        """res=[]
-        h={')':'(',']':'[','}':'{'}
-        for ele in s:
-            if ele=='(' or ele=='[' or ele=='{':
-                res.append(ele)
-            else:
-                if len(res) and res[-1]==h[ele]:
-                    res.pop()
-                else:
-                    return False
-        return len(res)==0"""
-
         
