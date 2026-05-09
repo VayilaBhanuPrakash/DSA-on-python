70bc2ab2ef18241282b502dd3d9fa059f2aea934
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-10 00:12:27 +0530

    Time: 4132 ms (5.95%) | Memory: 69.8 MB (18.91%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -4,14 +4,10 @@ class Solution:
             return 0
         a=[True for i in range(n)]
         a[0]=a[1]=False
-        for i in range(2,n+1):
+        for i in range(2,int(n**0.5)+1):
             for j in range(i+i,n,i):
                 a[j]=False
-        count=0
-        for i in range(n):
-            if a[i]==True:
-                count+=1
-        return count
+        return a.count(True)
         """count=1
         for i in range(3,n,2):
             flag=True
