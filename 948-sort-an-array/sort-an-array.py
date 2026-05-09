f066606f5d03552dd46a2de303da9de851e26dbc
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-09 15:32:53 +0530

    Time: 1007 ms (5.26%) | Memory: 26.9 MB (68.28%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,5 +1,40 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        nums.sort()
+        def merge(nums,l,mid,r):
+            c=[0 for i in range(r-l+1)]
+            i=l
+            j=mid+1
+            k=0
+            while i<=mid and j<=r:
+                if  nums[i]<nums[j]:
+                    c[k]=nums[i]
+                    i+=1
+                else:
+                    c[k]=nums[j]
+                    j+=1
+                k+=1
+            while i<=mid:
+                c[k]=nums[i]
+                i+=1
+                k+=1
+            while j<=r:
+                c[k]=nums[j]
+                j+=1
+                k+=1
+            k=0
+            index=l
+            while index<=r:
+                nums[index]=c[k]
+                index+=1
+                k+=1
+        def mergesort(nums,l,r):
+            if l<r:
+                mid=(l+r)//2
+                mergesort(nums,l,mid)
+                mergesort(nums,mid+1,r)
+                merge(nums,l,mid,r)
+        n=len(nums)
+        mergesort(nums,0,n-1)
         return nums
+
         
