20a133b1ea9223460dbcff2310af2a626bb278b1
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-12 12:02:50 +0530

    Time: 67 ms (34.65%) | Memory: 55.6 MB (60.40%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.py
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.py
@@ -1,7 +1,6 @@
 class Solution:
     def minimumEffort(self, tasks: List[List[int]]) -> int:
         tasks.sort(key=lambda x:x[1]-x[0],reverse=True)
-        print(tasks)
         sum=tasks[0][1]
         rem=tasks[0][1]-tasks[0][0]
         for i in range(1,len(tasks)):
