6f93eaf4f9150545008d3ab6819f87014b3dc51f
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-13 15:34:00 +0530

    Time: 5 ms (25.63%) | Memory: 20.2 MB (43.83%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -3,6 +3,6 @@ class Solution:
         n=len(triangle)
         for i in range(n-2,-1,-1):
             for j in range(i+1):
-                triangle[i][j]=triangle[i][j]+min(triangle[i+1][j],triangle[i+1][j+1])
+                triangle[i][j]=min(triangle[i+1][j]+triangle[i][j],triangle[i+1][j+1]+triangle[i][j])
         return triangle[0][0]
         
