d97f60c51188d3f52493f986a47987b34e15fce3
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-14 11:26:47 +0530

    Time: 2 ms (35.23%) | Memory: 19.2 MB (89.68%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.py
+++ 2892-check-if-array-is-good/check-if-array-is-good.py
@@ -1,14 +1,6 @@
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
-        nums.sort()
-        n=len(nums)
-        for i in range(1,n-1):
-            if i!=nums[i-1]:
-                return False
-        if nums[-1]!=n-1 or nums[-2]!=n-1:
-            return False
-        return True
-        """h={}
+        h={}
         n=len(nums)
         if n==1:
             return False
@@ -25,5 +17,15 @@ class Solution:
                     return False
         if n-1 in h and h[n-1]==2:
             return True
-        return False"""
+        return False
+        
+        """nums.sort()
+        n=len(nums)
+        for i in range(1,n-1):
+            if i!=nums[i-1]:
+                return False
+        if nums[-1]!=n-1 or nums[-2]!=n-1:
+            return False
+        return True"""
+        
         
