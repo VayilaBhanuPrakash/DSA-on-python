2f09c95ff7be0b6adaae89af36302b94cd32619e
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-17 11:05:29 +0530

    Time: 3 ms (98.63%) | Memory: 19.2 MB (92.60%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,7 +1,6 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
         def frequency(num):
-            num=str(num)
             count=1
             res=""
             for i in range(1,len(num)):
@@ -17,6 +16,5 @@ class Solution:
         res="1"
         for i in range(n-1):
             res=frequency(res)
-            print(res)
         return res
         
