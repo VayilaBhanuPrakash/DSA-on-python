0c6596165ecb75a1a366c908359b31a65ff138ac
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-19 15:01:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (39.00%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,6 +1,44 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        n=len(nums)
+        if n==1 and nums[0]==target:
+            return 0
         i=0
+        j=n-1
+        pivot=-1
+        while i<=j:
+            mid=i+(j-i)//2
+            if mid+1<n and nums[mid]>nums[mid+1]:
+                pivot=mid
+                break
+            elif nums[i]<=nums[mid]:
+                i=mid+1
+            else:
+                j=mid-1
+        print(pivot)
+        i=0
+        j=pivot
+        while i<=j:
+            mid=i+(j-i)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                i=mid+1
+            else:
+                j=mid-1
+        i=pivot+1
+        j=n-1
+        while i<=j:
+            mid=i+(j-i)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                i=mid+1
+            else:
+                j=mid-1
+        return -1
+
+        """i=0
         j=len(nums)-1
         while i<=j:
             mid=i+(j-i)//2
@@ -16,6 +54,7 @@ class Solution:
                     i=mid+1
                 else:
                     j=mid-1
-        return -1
+        return -1"""
+
             
            
