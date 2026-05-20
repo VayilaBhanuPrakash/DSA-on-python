05442c152204ffd88bacf7c56bfee129a1df49a2
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-20 07:49:37 +0530

    Time: 4 ms (28.41%) | Memory: 19.3 MB (81.78%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.py
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -4,30 +4,20 @@ class Solution:
         end=0
         for ele in nums:
             start=max(start,ele)
-            end=end+ele
-        #binary search
+            end+=ele
         while start<end:
-            #try for the middle as potential answer
             mid=start+(end-start)//2
-            #calculate how many pieces we can divide with is max sum
-            sum=0
+            summ=0
             pieces=1
             for ele in nums:
-                if sum+ele>mid:
-                    #we cannot add this to the subarray-->make new subarry
-                    #so new subarray consists one ele that is ele then sum=ele
+                summ+=ele
+                if summ>mid:
                     pieces+=1
-                    sum=ele
-                else:
-                    sum+=ele
+                    summ=ele
             if pieces>k:
                 start=mid+1
             else:
                 end=mid
         return start
-            
-
-
-        
 
         
