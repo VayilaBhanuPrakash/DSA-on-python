a553bf50a0777a5dda87004408b83b9bd3913de2
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-23 19:38:38 +0530

    Time: 23 ms (62.56%) | Memory: 24.6 MB (56.82%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -5,12 +5,23 @@ class Solution:
         for i in range(n):
             for j in range(n):
                 grid2[j].append(grid[i][j])
+        h={}
+        for ele in grid2:
+            t1=tuple(ele)
+            if t1 in h:
+                h[t1]+=1
+            else:
+                h[t1]=1
         res=0
         for ele in grid:
-            if ele in grid2:
-                res+=grid2.count(ele)
+            t2=tuple(ele)
+            if t2 in h:
+                res+=h[t2]
         return res
 
-        
+            
+
+
+                
             
         
