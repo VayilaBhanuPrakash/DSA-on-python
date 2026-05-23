a4d48e5bb67a1d51a8ee76ac7ddb0353b16cb353
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-23 18:10:43 +0530

    Time: 163 ms (25.68%) | Memory: 20.5 MB (53.20%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -0,0 +1,26 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        h1={}
+        h2={}
+        for ele in word1:
+            if ele in h1:
+                h1[ele]+=1
+            else:
+                h1[ele]=1
+        for ele in word2:
+            if ele in h2:
+                h2[ele]+=1
+            elif ele not in h1:
+                return False
+            else:
+                h2[ele]=1
+        if len(h1)!=len(h2):
+            return False
+        l=[]
+        for values in h1.values():
+            l.append(values)
+        for values in h2.values():
+            if values in l:
+                l.remove(values)
+        return len(l)==0
+        
