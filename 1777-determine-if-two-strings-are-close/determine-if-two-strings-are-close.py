6b844f78d7590009110db71f507d4cb488693ff3
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-23 18:42:03 +0530

    Time: 3 ms (99.98%) | Memory: 20.6 MB (18.90%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -1,30 +1,13 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        h1={}
-        h2={}
-        for ele in word1:
-            if ele in h1:
-                h1[ele]+=1
-            else:
-                h1[ele]=1
-        for ele in word2:
-            if ele in h2:
-                h2[ele]+=1
-            else:
-                h2[ele]=1
         l1=[]
-        l3=[]
-        for keys in h1:
-            l1.append(keys)
-            l3.append(h1[keys])
         l2=[]
-        l4=[]
-        for keys in h2:
-            l2.append(keys)
-            l4.append(h2[keys])
-        l1.sort()
-        l2.sort()
-        l3.sort()
-        l4.sort()
-        return l1==l2 and l3==l4
+        for i in range(97,123):
+            if chr(i) in word1 and chr(i) in word2:
+                l1.append(word1.count(chr(i)))
+                l2.append(word2.count(chr(i)))
+            elif (chr(i) in word1 and chr(i) not in word2) or (chr(i) not in word1 and chr(i) in word2):
+                return False
+        return sorted(l1)==sorted(l2)
+
         
