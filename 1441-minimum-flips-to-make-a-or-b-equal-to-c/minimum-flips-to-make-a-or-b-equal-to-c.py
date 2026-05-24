9c021a189e44e67c506eae4a6041fcd8d4cb6b18
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-05-24 23:33:52 +0530

    Time: 52 ms (11.03%) | Memory: 19.2 MB (40.90%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -3,10 +3,13 @@ class Solution:
         ab = bin(a)[2:]
         bb = bin(b)[2:]
         cb = bin(c)[2:]
+        n1=len(ab)
+        n2=len(bb)
+        n3=len(cb)
         count=0
-        for i in range(-1,-len(cb)-1,-1):
-            abi = int(ab[i]) if i >= -len(ab) else 0
-            bbi = int(bb[i]) if i >= -len(bb) else 0
+        for i in range(-1,-n3-1,-1):
+            abi = int(ab[i]) if i >= -n1 else 0
+            bbi = int(bb[i]) if i >= -n2 else 0
             cbi=int(cb[i])
             if cbi == 0:
                 if abi | bbi== 0:
@@ -20,17 +23,15 @@ class Solution:
                     pass
                 else:
                     count+=1
-        index=-len(cb)-1
-        while index>-len(ab)-1:
-            if ab[index]=="1":
-                count+=1
+        index = -n3-1
+        while index > -n1-1:
+            if ab[index] == "1":
+                count += 1
             index -= 1
-        index1 =- len(cb)-1
-        while index1 >- len(bb)-1:
+        index1 =- n3-1
+        while index1 >- n2-1:
             if bb[index1] == "1":
                 count += 1
             index1 -= 1
         return count
-            
-
-
+        
