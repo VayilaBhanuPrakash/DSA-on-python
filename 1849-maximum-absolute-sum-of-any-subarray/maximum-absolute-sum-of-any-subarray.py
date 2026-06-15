d7618783a7421f053ced3548da68f4050056e4ce
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-06-15 17:07:05 +0530

    Time: 39 ms (70.96%) | Memory: 31.8 MB (10.98%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
@@ -1,19 +1,16 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
+        psum = float('-inf')
         positive_sum = float('-inf')
-        positive_maxsum = float('-inf')
         for ele in nums:
-            positive_sum = max(positive_sum + ele , ele)
-            positive_maxsum = max(positive_sum,positive_maxsum)
-                
+            psum = max(psum + ele, ele)
+            positive_sum = max(positive_sum, psum)
 
+        nsum = float('inf')
         negative_sum = float('inf')
-        negetive_maxsum = float('inf')
         for ele in nums:
-            negative_sum = min(negative_sum + ele , ele)
-            negetive_maxsum = min(negative_sum,negetive_maxsum)
-
+            nsum = min(nsum + ele, ele)
+            negative_sum = min(negative_sum, nsum)
         
-        return max(positive_maxsum,-negetive_maxsum)
-
+        return max(positive_sum, -negative_sum)
         
