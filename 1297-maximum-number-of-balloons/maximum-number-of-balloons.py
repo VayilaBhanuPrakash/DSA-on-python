3a663b5ac7b6b2827e660d75b922cca411783072
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-06-22 16:31:56 +0530

    Time: 3 ms (61.87%) | Memory: 19.2 MB (95.12%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -6,15 +6,14 @@ class Solution:
                 h[letters] += 1
             else:
                 h[letters] = 1
-        if 'l' in h:
-            h['l'] = h['l'] // 2
-        if 'o' in h:
-            h['o'] = h['o'] // 2
         res = float('inf')
         word = "balon"
         for letters in word:
             if letters not in h:
                 return 0
-            res = min(res,h[letters])
+            elif letters == 'l' or letters == 'o':
+                res = min(res,h[letters]//2)
+            else:
+                res = min(res,h[letters])
         return res
         
