b95a0622cc9d9accc22642cad4a47d0f2e1b1ccd
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-06-24 12:19:10 +0530

    Time: 4 ms (61.84%) | Memory: 19.1 MB (90.45%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,47 +1,16 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        l=[]
-        nums=str(num)
-        n=len(nums)
-        for i in range(len(nums)):
-            val=int(nums[i])*(10**(n-1-i))
-            l.append(val)
+        values = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+        romans = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"]
         res = ""
-        for ele in l:
-            n = ele
-            r = ""
-            while n != 0:
-                if n >= 900:
-                    r = r + "M"
-                    n = n - 1000
-                elif n >= 400:
-                    r = r + "D"
-                    n = n - 500
-                elif n >= 90:
-                    r = r + "C"
-                    n = n - 100
-                elif n >= 40:
-                    r = r + "L"
-                    n = n - 50
-                elif n >= 9:
-                    r = r + "X"
-                    n = n - 10
-                elif n >= 4:
-                    r = r + "V"
-                    n = n - 5
-                elif n >= 1:
-                    r = r + "I"
-                    n = n - 1
-                elif n > -4:
-                    r = "I" + r
-                    n = n + 1
-                elif n > -40:
-                    r = "X" + r
-                    n = n + 10
-                elif n > -400:
-                    r = "C" + r
-                    n = n + 100
-            res = res + r
+        while num > 0:
+            for i in range(len(romans)-1,-1,-1):
+                if values[i] > num:
+                    continue
+                else:
+                    res = res + romans[i]
+                    num = num - values[i]
+                    break
         return res
+
         
