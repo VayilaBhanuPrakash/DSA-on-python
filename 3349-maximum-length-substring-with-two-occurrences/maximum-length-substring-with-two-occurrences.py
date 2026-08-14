22cfce1bd8bd61d253d323c4bad6b690fbce8ed9
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-08-15 02:05:36 +0530

    Time: 3 ms (76.42%) | Memory: 19.5 MB (5.42%) - LeetSync

---
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        maxx = 0
+        i = 0
+        j = 0
+        h = {}
+        while j < len(s):
+            if s[j] not in h:
+                h[s[j]] = 1
+            else:
+                h[s[j]] += 1
+                if h[s[j]] > 2:
+                    while h[s[j]] > 2:
+                        h[s[i]] -= 1
+                        i += 1
+            print(j-i+1)
+            maxx = max(maxx,j-i+1)
+            j += 1
+        return maxx
+
+
+
+
+
+        
+        
