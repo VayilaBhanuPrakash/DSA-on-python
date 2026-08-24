5a87f68c7907e1a125d35b5ba4ba026d0fca544b
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-08-24 13:38:33 +0530

    Time: 67 ms (5.28%) | Memory: 19.5 MB (96.14%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -8,8 +8,8 @@ class Solution:
         while i <= len(next) - 1:
             visit.append(next[i])
             curr = next[i]
-            for j in range(0,len(rooms)):
-                if j in rooms[curr] and j not in visit:
+            for j in rooms[curr]:
+                if j not in visit:
                     next.append(j)
             i += 1
         for i in range(1,len(rooms)):
