e72184c126a6d09a2d94e1e4513f60a8a3d9e930
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-09-11 22:10:59 +0530

    Time: 16 ms (61.32%) | Memory: 19.3 MB (72.53%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -1,6 +1,6 @@
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        res = []
+        res = set()
 
         for i in range(len(digits)):
             if digits[i] == 0:
@@ -11,7 +11,7 @@ class Solution:
                 for k in range(len(digits)):
                     if k == i or k == j:
                         continue
-                    if digits[k] % 2 == 0 and [digits[i],digits[j],digits[k]] not in res:
-                        res.append([digits[i],digits[j],digits[k]])
+                    if digits[k] % 2 == 0:
+                        res.add(digits[i]*100 + digits[j]*10 + digits[k])
         return len(res)
         
