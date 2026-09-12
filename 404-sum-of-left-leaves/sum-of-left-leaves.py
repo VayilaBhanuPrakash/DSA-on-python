85deae694400afa4e5c94955d7a86cec72e2d594
VayilaBhanuPrakash <bhanuprakash12210@gmail.com>
2026-09-12 22:52:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (26.38%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        def leaf(node):
+            nonlocal res
+            if node == None:
+                return
+            leaf(node.left)
+            if node.left and node.left.left == None and node.left.right == None:
+                res += node.left.val
+            leaf(node.right)
+        leaf(root)
+        return res
+        
