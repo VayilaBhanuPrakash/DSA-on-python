803bb65090b76e5f4e53c2e7ea178c37e9053c57
Vayila Bhanu Prakash <bhanuprakash12210@gmail.com>
2026-09-26 11:23:31 +0530

    Time: 68 ms (17.76%) | Memory: 51.4 MB (81.62%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
@@ -1,26 +1,21 @@
 class Solution:
     def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
         res = ""
+        stack = []
         h = {}
         for i in range(len(knowledge)):
             h[knowledge[i][0]] = knowledge[i][1]
-
-        start = -1
-        end = -1
-        for i in range(len(s)):
-            if s[i] == ")":
-                key = "".join(s[start:end])
-
-                res = res + h.get(key,"?")
-                start = -1
-                end = -1
-    
-            elif s[i].islower() and start == -1 and end == -1:
-                res = res + s[i]
-            elif s[i] == "(":
-                start = i + 1
-                end = i + 1
+        for ele in s:
+            if ele == ")":
+                key = "".join(stack[1:])
+                if key in h:
+                    res = res + h[key]
+                else:
+                    res = res + "?"
+                stack = []
+            elif ele.islower() and not stack:
+                res = res + ele
             else:
-                end = end + 1
+                stack.append(ele)
         return res
         
